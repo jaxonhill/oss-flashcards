ca687b7f9a7b5dbe754e2d444a4e15b59a86291a
Jaxon Hill <jaxoncharleshill@gmail.com>
2025-07-19 21:47:29 -0700

    Add separate card screen component

--- frontend/src/features/card-screen/components/card-screen.tsx
+++ frontend/src/features/card-screen/components/card-screen.tsx
@@ -1,3 +1,10 @@
+import Flashcard from "./flashcard";
+import { FLASHCARDS } from "../config/config";
+
 export default function CardScreen() {
-	return <div></div>;
+	return (
+		<div className="px-64 flex flex-col gap-6 w-full pt-24">
+			<Flashcard flashcard={FLASHCARDS[0]} />
+		</div>
+	);
 }
