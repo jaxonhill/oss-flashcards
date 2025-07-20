d6ab143207f48a08c2b68a33432d80670de091c9
Jaxon Hill <jaxoncharleshill@gmail.com>
2025-07-19 22:18:19 -0700

    Add difficulty buttons to card screen

--- frontend/src/features/card-screen/components/card-screen.tsx
+++ frontend/src/features/card-screen/components/card-screen.tsx
@@ -1,10 +1,21 @@
 import Flashcard from "./flashcard";
-import { FLASHCARDS } from "../config/config";
+import { FLASHCARDS, DIFFICULTIES } from "../config/config";
+import DifficultyButton from "./difficulty-button";
 
 export default function CardScreen() {
 	return (
 		<div className="px-64 flex flex-col gap-6 w-full pt-24">
 			<Flashcard flashcard={FLASHCARDS[0]} />
+			<div className="flex gap-4">
+				{DIFFICULTIES.map((difficulty) => {
+					return (
+						<DifficultyButton
+							difficulty={difficulty}
+							onClick={() => {}}
+						/>
+					);
+				})}
+			</div>
 		</div>
 	);
 }
