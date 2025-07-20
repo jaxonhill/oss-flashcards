276eda8aff5b09a9125d454f011b668f0954fb32
Jaxon Hill <jaxoncharleshill@gmail.com>
2025-07-19 17:16:47 -0700

    Add flashcards to new file

---
+++ frontend/src/features/card-screen/ config/config.ts
@@ -0,0 +1,12 @@
+import type { Flashcard } from "../types/types";
+
+export const FLASHCARDS: Flashcard[] = [
+	{
+		front: "front of the card 1",
+		back: "back of the card 1",
+	},
+	{
+		front: "front of the card 2",
+		back: "back fo the card 2",
+	},
+];
