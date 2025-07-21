d34a11d19959d8d0971ff33ae98caaccda1d243c
Jaxon Hill <jaxoncharleshill@gmail.com>
2025-07-21 15:18:05 -0700

    Fix spacing with default card

--- frontend/src/features/card-screen/components/default-card.tsx
+++ frontend/src/features/card-screen/components/default-card.tsx
@@ -7,9 +7,9 @@ export default function DefaultFlashcard({
 	actionButtons,
 }: DefaultFlashcardProps) {
 	return (
-		<div className="h-[576px] p-8 flex justify-center items-center text-3xl text-white bg-zinc-800 border border-zinc-700 rounded-2xl relative">
+		<div className="h-[512px] p-8 flex justify-center items-center text-3xl text-white bg-zinc-800 border border-zinc-700 rounded-2xl relative">
 			{contentSection}
-			<div className="bg-zinc-800 flex items-center gap-3 p-4 absolute bottom-0 w-full rounded-2xl justify-end">
+			<div className="bg-zinc-800 flex items-center gap-1.5 p-4 absolute bottom-0 w-full rounded-2xl justify-end">
 				{actionButtons}
 			</div>
 		</div>
