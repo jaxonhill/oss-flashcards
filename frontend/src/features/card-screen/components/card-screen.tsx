4ef370db5758ca9b95a2a8b59c94fedd5a94cadf
Jaxon Hill <jaxoncharleshill@gmail.com>
2025-07-21 15:18:19 -0700

    Add progress bar and fix styling on card screen

--- frontend/src/features/card-screen/components/card-screen.tsx
+++ frontend/src/features/card-screen/components/card-screen.tsx
@@ -1,20 +1,32 @@
-import Flashcard from "./flashcard";
 import { FLASHCARDS, DIFFICULTIES } from "../config/config";
 import DifficultyButton from "./difficulty-button";
+import FrontBackCard from "./cards/front-back-card";
+import ProgressBar from "./progress-bar";
 
 export default function CardScreen() {
+	const deckName: string = "College Algebra";
+	const currentCardNumber: number = 12;
+	const totalCards: number = 40;
+	const label: string = `${deckName} - ${currentCardNumber}/${totalCards}`;
 	return (
-		<div className="px-64 flex flex-col gap-6 w-full pt-24">
-			<Flashcard flashcard={FLASHCARDS[0]} />
-			<div className="flex gap-4">
-				{DIFFICULTIES.map((difficulty) => {
-					return (
-						<DifficultyButton
-							difficulty={difficulty}
-							onClick={() => {}}
-						/>
-					);
-				})}
+		<div className="px-64 pt-14 flex flex-col gap-6">
+			<ProgressBar
+				label={label}
+				currentCardNumber={currentCardNumber}
+				totalNumberOfCards={totalCards}
+			/>
+			<div className="flex flex-col gap-3 w-full">
+				<FrontBackCard flashcard={FLASHCARDS[0]} />
+				<div className="flex gap-3">
+					{DIFFICULTIES.map((difficulty) => {
+						return (
+							<DifficultyButton
+								difficulty={difficulty}
+								onClick={() => {}}
+							/>
+						);
+					})}
+				</div>
 			</div>
 		</div>
 	);
