00f1032100d1cb4f11e749c3689a241a6b29d514
Jaxon Hill <jaxoncharleshill@gmail.com>
2025-07-23 13:20:33 -0700

    Remove all separate components from ClozeCard

--- frontend/src/features/cloze-card/components/cloze-card.tsx
+++ frontend/src/features/cloze-card/components/cloze-card.tsx
@@ -1,7 +1,7 @@
 import { tokenizeClozeCardText } from "../utils/cloze-parsing";
 import type { ClozeNode } from "../types/types";
 import DefaultCard from "../../flashcards/components/default-card";
-import { Eye } from "lucide-react";
+import { ClozeCardContent } from "./cloze-card-content";
 
 // prettier-ignore
 const sampleText: string = "Einstein was born in {{c1::1879}} and published his theory in {{c2::1905}}.";
@@ -22,42 +22,5 @@ export default function ClozeCard({ revealGroup }: ClozeCardProps) {
 			}
 			actionButtons={[]}
 		/>
-	);
-}
-
-interface ClozeCardContentProps {
-	clozeCardTokens: (ClozeNode | string)[];
-	revealGroup: number;
-}
-
-function ClozeCardContent({
-	clozeCardTokens,
-	revealGroup,
-}: ClozeCardContentProps) {
-	return (
-		<p>
-			{clozeCardTokens.map((token, i) => {
-				if (typeof token === "string") {
-					return <span key={i}>{token}</span>;
-				} else {
-					const isRevealGroup: boolean = revealGroup === token.group;
-					if (isRevealGroup) {
-						return (
-							<button
-								key={i}
-								className="bg-zinc-700 select-none rounded px-2 relative inline-flex justify-center items-center w-fit"
-							>
-								<Eye className="size-6 text-zinc-300 absolute" />
-								<span className="invisible">
-									{token.content}
-								</span>
-							</button>
-						);
-					} else {
-						return <span key={i}>{token.content}</span>;
-					}
-				}
-			})}
-		</p>
 	);
 }
