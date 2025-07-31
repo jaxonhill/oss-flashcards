5cf0c9716c540e11978cefdfb0a7df0f416118d3
Jaxon Hill <jaxoncharleshill@gmail.com>
2025-07-30 23:54:19 -0700

    Add React Router and add basic pages

--- frontend/src/app/main.tsx
+++ frontend/src/app/main.tsx
@@ -1,10 +1,24 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import { BrowserRouter, Routes, Route } from "react-router";
+
+import LandingPage from "../pages/landing-page";
+import DashboardPage from "../pages/dashboard/dashboard-page";
+import DeckListPage from "../pages/decks/deck-list-page";
+import CreateNewDeckPage from "../pages/decks/new/create-new-deck-page";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<App />
+		<BrowserRouter>
+			<Routes>
+				<Route index element={<LandingPage />} />
+				<Route path="dashboard" element={<DashboardPage />} />
+				<Route path="decks">
+					<Route index element={<DeckListPage />} />
+					<Route path="new" element={<CreateNewDeckPage />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
 	</StrictMode>
 );
